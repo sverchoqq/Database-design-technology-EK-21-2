007da93c00edbc5abc8ff9eac7b1048493d81bbb
Sergei <st1488171@gmail.com>
2024-05-28 15:12:13 +0300

    trosko lab3

---
+++ ЛР3/trosko_lab3.sql
@@ -0,0 +1,6 @@
+﻿USE STUDENTS;
+INSERT INTO PersonalInfo (StudentID, FirstName, LastName, DateOfBirth)
+VALUES 
+    (1, 'Олександр', 'Петров', '2000-05-15'),
+    (2, 'Марія', 'Іваненко', '2001-03-22'),
+    (3, 'Василь', 'Коваленко', '1999-10-10');
